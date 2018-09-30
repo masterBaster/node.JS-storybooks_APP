abeb5f7167486c4f0baf8b8a898b015fc3b0da89
masterBaster <bartek.baster@gmail.com>
2018-09-30 12:14:28 +0200

    prepared our repository

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:'mongodb://masterBaster:version01@ds119662.mlab.com:19662/storybooks-dev',
+  googleClientID:'269268250532-slrjegc1fiqp154vf3juhu55igcf01c5.apps.googleusercontent.com',
+  googleClientSecret:'brfy3-uPI0TwfLFUVn0mBhYG'
+}

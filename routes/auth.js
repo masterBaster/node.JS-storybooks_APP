4219d922bf73b2e44db681b365b7d609b525f107
masterBaster <bartek.baster@gmail.com>
2018-09-30 19:57:33 +0200

    set up google oauth

--- routes/auth.js
+++ routes/auth.js
@@ -5,4 +5,11 @@ const passport = require('passport')
 router.get('/google', 
   passport.authenticate('google', {scope: ['profile', 'email']}));
 
+router.get('/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
 module.exports = router;

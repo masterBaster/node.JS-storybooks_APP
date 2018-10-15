2b07b54f66b7bb72564a7cad29bc8e2fca0cfcd9
masterBaster <bartek.baster@gmail.com>
2018-10-15 11:00:15 +0200

    showed templates and formatted date using moment.js

--- app.js
+++ app.js
@@ -25,7 +25,8 @@ const keys = require('./config/keys');
 //Handlebars helpers
 const {
   truncate, 
-  stripTags
+  stripTags, 
+  formatDate
 } = require('./helpers/hbs');
 
 //Map global Promises
@@ -45,7 +46,8 @@ app.use(bodyParser.json());
 app.engine('handlebars', exphbs({
   helpers: {
     truncate: truncate,
-    stripTags: stripTags
+    stripTags: stripTags, 
+    formatDate: formatDate
   },
   defaultLayout: 'main'
 }));
--- routes/stories.js
+++ routes/stories.js
@@ -1,33 +1,48 @@
 const express = require('express');
 const router = express.Router();
-const moongose = require('mongoose');
-const Story = moongose.model('stories');
-const User = moongose.model('users');
+const mongoose = require('mongoose');
+const Story = mongoose.model('stories');
+const User = mongoose.model('users');
 const { ensureAuthenticated, ensureGuest } = require('../helpers/auth');
 
-
+// Stories Index
 router.get('/', (req, res) => {
-  Story.find({status:'public'})
-  .populate('user')
-  .then(stories => {
-    res.render('stories/index', {
-      stories: stories
+  Story.find({
+    status: 'public'
+  })
+    .populate('user')
+    .then(stories => {
+      res.render('stories', {
+        stories: stories
+      });
     });
-  });
 });
 
+// Show Single Story
+router.get('/show/:id', (req, res) => {
+  Story.findOne({
+    _id: req.params.id
+  })
+    .populate('user')
+    .then(story => {
+      res.render('stories/show', {
+        story: story
+      });
+    });
+});
+
+// Add Story Form
 router.get('/add', ensureAuthenticated, (req, res) => {
   res.render('stories/add');
 });
 
-//Process and story
-
+// Process Add Story
 router.post('/', (req, res) => {
   let allowComments;
 
-  if(req.body.allowComments){
+  if (req.body.allowComments) {
     allowComments = true;
-  }else{
+  } else {
     allowComments = false;
   }
 
@@ -36,24 +51,15 @@ router.post('/', (req, res) => {
     body: req.body.body,
     status: req.body.status,
     allowComments: allowComments,
-    user: req.body.id
+    user: req.user.id
   }
 
-  //create story
+  // Create Story
   new Story(newStory)
     .save()
     .then(story => {
-      res.redirect(`/stories/show/${story.id}`)
-    })
+      res.redirect(`/stories/show/${story.id}`);
+    });
 });
-
-router.get('/edit', (req, res) => {
-  res.render('stories/edit');
-});
-
-router.get('/show', (req, res) => {
-  res.render('stories/show');
-});
-
 
 module.exports = router;
